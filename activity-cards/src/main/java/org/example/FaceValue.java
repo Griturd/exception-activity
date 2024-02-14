b72cb089edf4021aaa672d27a484f9f74f6c993d
Griffin <156863205+Griturd@users.noreply.github.com>
2024-02-13 20:04:37 -0700

    Delete activity-cards/src/main/java/org/example directory

--- activity-cards/src/main/java/org/example/FaceValue.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public enum FaceValue {
-
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
-}
--- activity-cards/src/main/java/org/example/Suit.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public enum Suit {
-
-    CLUBS, DIAMONDS, HEARTS, SPADES
-}
